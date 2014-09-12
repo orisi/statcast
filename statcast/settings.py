b90dd07387a20774cc746edabd49e7cc6767675c
Kuba Kucharski <kuba.kucharski@gmail.com>
2014-09-12 22:31:38 +0200

    settings

---
+++ statcast/settings.py
@@ -0,0 +1,101 @@
+"""
+Django settings for tutorial project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '2!#(pr!a)6p023x^t(^9_-!&u0@z^jix5xw_7=!wpk0#d=smha'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'monitor',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'statcast.urls'
+
+WSGI_APPLICATION = 'statcast.wsgi.application'
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'Europe/Warsaw'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = '/Users/kuba/Documents/messages'
+MEDIA_URL = "storage/"
+
+
+# IN CASE OF DDOS ATTEMPT
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '186400/day',
+        # equals 1 every second
+
+
+    }
+}
---
+++ statcast/urls.py
@@ -0,0 +1,44 @@
+from django.conf.urls import patterns, url, include
+from rest_framework import routers, permissions
+from monitor import views
+
+#router = routers.DefaultRouter()
+#router.register(r'users', views.UserViewSet)
+#router.register(r'groups', views.GroupViewSet)
+
+#router.register(r'messages', views.MsgViewSet)
+
+from rest_framework.urlpatterns import format_suffix_patterns
+
+import settings
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browseable API.
+
+urlpatterns = patterns('',
+
+        # unhash this to get access to json user/group api
+        #url(r'^meta/', include(router.urls)),
+        url(r'^storage/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+
+        url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+        )
+
+
+urlpatterns_to_suffix = patterns('',
+
+        url(r'^log/(?P<pk>[0-9]+)/$', views.MonitorDetail.as_view()),
+        url(r'^logs/', views.MonitorList.as_view()),
+        url(r'^last/', views.MonitorListLast.as_view()),
+
+         url(r'^$', views.MonitorList.as_view()),
+       
+        )
+ 
+ 
+urlpatterns_suffixed = format_suffix_patterns(urlpatterns_to_suffix)
+
+urlpatterns += urlpatterns_suffixed
+
+
